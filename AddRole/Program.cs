5aa6e76dd9a479ace9fa5393eb1e93b8e961735e
Dima <d.a.shimanov@gmail.com>
2017-09-30 22:58:25 +0700

    Добавление роли sysadmin пользователю sql server

---
+++ AddRole/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace AddRole
+{
+    class Program
+    {
+        static int Main()
+        {
+            SqlConnection sqlConnection = new SqlConnection("Server=localhost; Initial Catalog=master; Integrated Security = SSPI;");
+            SqlCommand command = new SqlCommand
+            {
+                CommandText = "ALTER SERVER ROLE [sysadmin] ADD MEMBER [NT AUTHORITY\\СИСТЕМА]",
+                CommandType = CommandType.Text,
+                Connection = sqlConnection
+            };
+
+            try
+            {
+                sqlConnection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                object result = null;
+                while (reader.Read())
+                {
+                    result = reader.GetValue(0);
+                }
+                sqlConnection.Close();
+
+                if (result == null)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            catch (Exception e)
+            {
+                return 100;
+            }
+        }
+    }
+}
